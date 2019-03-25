a2921fa24b27699f673ee2564be2611393df4902
Hamza <37183538+lepetitpatron@users.noreply.github.com>
2019-03-25 14:48:47 +0100

    opdracht afgewerkt

--- django_examen/mysite/tijd/views.py
+++ django_examen/mysite/tijd/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import string
+import datetime
 
 # Create your views here.
 def brexit(request):    
-    output = '<h1>Brexit means brexit</h1>'
-   
+    now = datetime.datetime.now()
+    brexit = datetime.datetime(2019,3,30,0,0,0)
+    result = (brexit - now)
+
+    seconds = result.total_seconds()
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+
+    output = '<p>'+str(hours)+':'+str(minutes)+':'+str(seconds)+'</p>'
+
     return HttpResponse(output)
