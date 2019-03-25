1ec21d17667c26857f487e97a78e269609d2dee8
Hamza <37183538+lepetitpatron@users.noreply.github.com>
2019-03-25 14:22:32 +0100

    View brexit added

--- django_examen/mysite/tijd/views.py
+++ django_examen/mysite/tijd/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import string
 
 # Create your views here.
+def brexit(request):    
+    output = '<h1>Brexit means brexit</h1>'
+   
+    return HttpResponse(output)
